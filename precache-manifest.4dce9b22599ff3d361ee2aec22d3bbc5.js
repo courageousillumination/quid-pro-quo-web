6b8c891d204f233efe0ece2b9b98775fae625d71
Tristan Rasmussen <tristanrasmussen@tristanrasmussen.com>
2020-07-02 06:56:31 -0700

    Updates

---
+++ precache-manifest.4dce9b22599ff3d361ee2aec22d3bbc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496dd00b8ba17e47921569133dd40ef2",
+    "url": "/quid-pro-quo-web/index.html"
+  },
+  {
+    "revision": "930ffff8dbe296ca2d30",
+    "url": "/quid-pro-quo-web/static/js/2.3ffee270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quid-pro-quo-web/static/js/2.3ffee270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56bbaf1562eee827504",
+    "url": "/quid-pro-quo-web/static/js/main.59b6d9a4.chunk.js"
+  },
+  {
+    "revision": "477e63ec061bb5e02428",
+    "url": "/quid-pro-quo-web/static/js/runtime-main.55562629.js"
+  }
+]);
